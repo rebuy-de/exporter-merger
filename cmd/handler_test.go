5ef26a7a0a53243d1b12c0ea1fcaccd787155d0e
Phsm Qwerty <me@nek0.net>
2019-04-12 13:16:58 +0200

    Fixed tests to be order-unaware

--- cmd/handler_test.go
+++ cmd/handler_test.go
@@ -2,14 +2,28 @@ package cmd_test
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"sort"
 	"testing"
 
+	"github.com/prometheus/common/expfmt"
 	"github.com/rebuy-de/exporter-merger/cmd"
 	log "github.com/sirupsen/logrus"
 )
+
+func Equal(a, b []float64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
 
 func testExporter(t testing.TB, content string) (string, func()) {
 	t.Helper()
@@ -50,27 +64,77 @@ func TestHandler(t *testing.T) {
 		t.Fatalf("Received non-200 response: %d\n", resp.StatusCode)
 	}
 
-	want := `# TYPE bar untyped
-bar 4
-# TYPE conflict untyped
-conflict 2
-conflict 5
-# TYPE foo untyped
-foo 1
-# TYPE shared untyped
-shared{meh="a"} 3
-shared{meh="b"} 6
-`
-	have, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
+	// 	want := `# TYPE bar untyped
+	// bar 4
+	// # TYPE conflict untyped
+	// conflict 2
+	// conflict 5
+	// # TYPE foo untyped
+	// foo 1
+	// # TYPE shared untyped
+	// shared{meh="a"} 3
+	// shared{meh="b"} 6
+	// `
+	// have, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	eFmt := new(expfmt.TextParser)
+	part, err := eFmt.TextToMetricFamilies(resp.Body)
+
+	fooWanted := 1.0
+	var foo float64
+
+	barWanted := 4.0
+	var bar float64
+
+	var conflictWanted sort.Float64Slice = []float64{2.0, 5.0}
+	var conflict sort.Float64Slice = make([]float64, 0)
+
+	sharedWanted := map[string]float64{"a": 3.0, "b": 6.0}
+	shared := make(map[string]float64)
+
+	for n, mf := range part {
+		if n == "bar" {
+			bar = mf.GetMetric()[0].GetUntyped().GetValue()
+		}
+
+		if n == "foo" {
+			foo = mf.GetMetric()[0].GetUntyped().GetValue()
+		}
+
+		if n == "conflict" {
+			for _, metric := range mf.GetMetric() {
+				conflict = append(conflict, metric.GetUntyped().GetValue())
+			}
+		}
+
+		if n == "shared" {
+			for _, metric := range mf.GetMetric() {
+				label := metric.GetLabel()[0].GetValue()
+				value := metric.GetUntyped().GetValue()
+				shared[label] = value
+			}
+		}
 	}
 
-	if want != string(have) {
-		t.Error("Got wrong response.")
-		t.Error("Want:")
-		t.Error(want)
-		t.Error("Have:")
-		t.Error(string(have))
+	if bar != barWanted {
+		t.Errorf("bar is %f but wanted %f", bar, barWanted)
+	}
+
+	if foo != 1.0 {
+		t.Errorf("foo is %f but wanted %f", foo, fooWanted)
+	}
+
+	conflictWanted.Sort()
+	conflict.Sort()
+
+	if !Equal(conflict, conflictWanted) {
+		t.Errorf("conflict is %v but wanted %v", conflict, conflictWanted)
+	}
+
+	if !reflect.DeepEqual(shared, sharedWanted) {
+		t.Errorf("shared is %v but wanted %v", shared, sharedWanted)
 	}
 }
